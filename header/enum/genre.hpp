9203f102d4473c5bb0cca9c9d1cefeab18e8a4a1
jzells09 <justinzeller2004@gmail.com>
2025-03-29 23:46:43 -0500

    Started making header files, and enum for genres

---
+++ header/enum/genre.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+enum class Genre
+{
+    Action,
+    Adventure,
+    Animation,
+    Children_s,
+    Comedy,
+    Crime,
+    Documentary,
+    Drama,
+    Fantasy,
+    Film_Noir,
+    Horror,
+    Musical,
+    Mystery,
+    Romance,
+    Sci_Fi,
+    Thriller,
+    War,
+    Western,
+};
---
+++ header/user.hpp
@@ -0,0 +1,13 @@
+class User
+{
+private:
+
+public:
+};
+
+// user should have following functions:
+// create user (user info is saved to  file)
+// modify/add watched movies
+// modify/add genre prefs
+// generate genre prefs from list of movies watched
+// maybe get list of actors? etc
